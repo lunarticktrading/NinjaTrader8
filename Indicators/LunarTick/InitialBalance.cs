cc9eb40c87d76738b5eefa6b61ac7fa0f4ef6ac6
Charles Gamble <gambcl@proton.me>
2026-02-09 11:08:00 +0000

    Fixed issue in Initial Balance that stopped plots extending past Friday close

--- Indicators/LunarTick/InitialBalance.cs
+++ Indicators/LunarTick/InitialBalance.cs
@@ -132,7 +132,7 @@ namespace NinjaTrader.NinjaScript.Indicators.LunarTick
 
         #region Constants
 
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         #endregion
 
@@ -505,7 +505,7 @@ namespace NinjaTrader.NinjaScript.Indicators.LunarTick
                 }
 
                 // Populate plots (only after IB region is confirmed).
-                if (currentIBRegion.IsConfirmed && (Time[0] > currentIBRegion.StartTime) && (Time[0] <= currentIBRegion.StartTime.AddDays(1)) && (CurrentBar > _lastBarPlotted))
+                if (currentIBRegion.IsConfirmed && (Time[0] > currentIBRegion.StartTime) && (CurrentBar > _lastBarPlotted))
                 {
                     int startBarsAgo = 0;
                     if (!currentIBRegion.IsBackFilled)
