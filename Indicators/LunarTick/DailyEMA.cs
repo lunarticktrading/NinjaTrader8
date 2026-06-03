36d7ff4af9e5b43c87d49bd7a4e6e6d31c76ec20
Charles Gamble <gambcl@proton.me>
2026-06-03 12:39:20 +0100

    Added DailySMA, DailyEMA indicators

---
+++ Indicators/LunarTick/DailyEMA.cs
@@ -0,0 +1,281 @@
+#region Using declarations
+using NinjaTrader.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Windows.Media;
+using System.Xml.Serialization;
+#endregion
+
+//This namespace holds Indicators in this folder and is required. Do not change it. 
+namespace NinjaTrader.NinjaScript.Indicators.LunarTick
+{
+    [Gui.CategoryOrder("Parameters", 1)]
+    [Gui.CategoryOrder("Developer", 2)]
+    public class DailyEMA : Indicator
+    {
+        internal class DailyValue
+        {
+            public DailyValue(DateTime barStartTime, DateTime barEndTime, int barIndex, double value)
+            {
+                BarStartTime = barStartTime;
+                BarEndTime = barEndTime;
+                BarIndex = barIndex;
+                Value = value;
+            }
+
+            public DateTime BarStartTime { get; }
+            public DateTime BarEndTime { get; }
+            public int BarIndex { get; }
+            public double Value { get; }
+        }
+
+        #region Constants
+
+        public const string Version = "1.0.0";
+
+        #endregion
+
+        #region Members
+
+        private EMA _dailyEMA;
+        private Dictionary<int, DailyValue> _historicDailyValues = new();
+        private bool _backfillHistoricBars = false;
+        private double _lastDailyValue = 0;
+        private int _firstBarOfSession = 0;
+
+        #endregion
+
+        #region Properties
+
+        [NinjaScriptProperty]
+        [Range(1, int.MaxValue)]
+        [Display(Name = "Period", Order = 1, GroupName = "Parameters")]
+        public int Period
+        { get; set; }
+
+        [ReadOnly(true)]
+        [XmlIgnore]
+        [Display(Name = "Version", Description = "Version information.", Order = 1, GroupName = "Developer")]
+        public string VersionInformation
+        { get; set; }
+
+        [Display(Name = "Debug", Description = "Toggle debug logging.", Order = 2, GroupName = "Developer")]
+        public bool Debug
+        { get; set; }
+
+        [Browsable(false)]
+        [XmlIgnore]
+        public Series<double> Value
+        {
+            get { return Values[0]; }
+        }
+
+        #endregion
+
+        #region Indicator methods
+
+        protected override void OnStateChange()
+        {
+            DebugPrint($"OnStateChange({State})");
+
+            if (State == State.SetDefaults)
+            {
+                Description = @"Plots the daily EMA on intraday charts.";
+                Name = "DailyEMA";
+                Calculate = Calculate.OnPriceChange;
+                IsOverlay = true;
+                DisplayInDataBox = true;
+                DrawOnPricePanel = true;
+                DrawHorizontalGridLines = true;
+                DrawVerticalGridLines = true;
+                PaintPriceMarkers = true;
+                ScaleJustification = NinjaTrader.Gui.Chart.ScaleJustification.Right;
+                IsAutoScale = false;
+                //Disable this property if your indicator requires custom values that cumulate with each new market data event. 
+                //See Help Guide for additional information.
+                IsSuspendedWhileInactive = true;
+
+                Period = 1;
+
+                VersionInformation = $"{Version} - {Assembly.GetAssembly(typeof(DailyEMA)).GetName().Version}";
+                Debug = false;
+
+                AddPlot(Brushes.White, "DailyEMA");
+            }
+            else if (State == State.Configure)
+            {
+                AddDataSeries(Data.BarsPeriodType.Day, 1);
+
+                _historicDailyValues.Clear();
+                _backfillHistoricBars = false;
+                _lastDailyValue = 0.0;
+            }
+            else if (State == State.DataLoaded)
+            {
+                _dailyEMA = EMA(BarsArray[1], Period);
+            }
+            else if (State == State.Transition)
+            {
+                _backfillHistoricBars = true;
+            }
+        }
+
+        protected override void OnBarUpdate()
+        {
+            if (BarsInProgress == 0)
+            {
+                // Chart bars.
+
+                if (!Bars.BarsType.IsIntraday)
+                    return;
+
+                if (Bars.IsFirstBarOfSession)
+                    _firstBarOfSession = CurrentBar;
+
+                if (State == State.Realtime)
+                {
+                    if (_backfillHistoricBars)
+                    {
+                        // Backfill historic bars.
+                        DebugPrint("Backfilling historic bars");
+                        int dailyBarIndex = 0;
+                        for (int i = 1; i < CurrentBar; i++)
+                        {
+                            int barsAgo = CurrentBar - i;
+                            DateTime barStartTime = Time[barsAgo + 1];
+                            if (TryGetDailyValue(barStartTime, dailyBarIndex, out DailyValue dailyValue))
+                            {
+                                Value[barsAgo] = dailyValue.Value;
+                                dailyBarIndex = dailyValue.BarIndex; // Start from here next lookup, to save time.
+                                DebugPrint($"Backfilling: i={i}, barsAgo={barsAgo}, barStartTime={barStartTime}, dailyBarIndex={dailyBarIndex}, historicValue={dailyValue.Value}");
+                            }
+                        }
+                        _backfillHistoricBars = false;
+                    }
+                    Value[0] = _lastDailyValue;
+                    // Plot all DailyEMA values for the current day.
+                    for (int barsAgo = 0; barsAgo <= CurrentBar - _firstBarOfSession; barsAgo++)
+                        Value[barsAgo] = _lastDailyValue;
+                }
+            }
+            else if (BarsInProgress == 1)
+            {
+                // Daily bars.
+
+                if (State == State.Historical)
+                {
+                    // Called once at close of historic bar, so just record daily EMA value.
+                    if (CurrentBar > 0)
+                    {
+                        _dailyEMA.Update();
+                        _historicDailyValues[CurrentBar] = new DailyValue(Time[1], Time[0], CurrentBar, _dailyEMA[0]);
+                        DebugPrint($"Recorded historical DailyEMA [{CurrentBar}][{Time[0]}] = [{_dailyEMA[0]}]");
+                    }
+                }
+                else if (State == State.Realtime)
+                {
+                    _dailyEMA.Update();
+                    _lastDailyValue = _dailyEMA[0];
+                }
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private bool TryGetDailyValue(DateTime dt, int startIndex, out DailyValue result)
+        {
+            result = null;
+
+            // Starting from startIndex, look for which Daily bar contains dt.
+            for (int i = Math.Max(startIndex, 0); i < _historicDailyValues.Count; i++)
+            {
+                if (_historicDailyValues.TryGetValue(i, out var currDailyValue) && dt >= currDailyValue.BarStartTime && dt < currDailyValue.BarEndTime)
+                {
+                    // Found matching Daily bar.
+                    DebugPrint($"TryGetDailyValue({dt}, {startIndex}): i={i}, dailyBarStartDateTime={currDailyValue.BarStartTime}, dailyBarEndDateTime={currDailyValue.BarEndTime}");
+                    result = currDailyValue;
+                    return true;
+                }
+            }
+
+            // Not found.
+            result = null;
+            return false;
+        }
+
+        private void DebugPrint(string msg)
+        {
+            if (Debug)
+            {
+                if (Instrument != null && !string.IsNullOrWhiteSpace(Instrument.FullName))
+                    Print($"DailyEMA[{Instrument.FullName}]: {msg}");
+                else
+                    Print($"DailyEMA: {msg}");
+            }
+        }
+
+        #endregion
+    }
+}
+
+#region NinjaScript generated code. Neither change nor remove.
+
+namespace NinjaTrader.NinjaScript.Indicators
+{
+	public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
+	{
+		private LunarTick.DailyEMA[] cacheDailyEMA;
+		public LunarTick.DailyEMA DailyEMA(int period)
+		{
+			return DailyEMA(Input, period);
+		}
+
+		public LunarTick.DailyEMA DailyEMA(ISeries<double> input, int period)
+		{
+			if (cacheDailyEMA != null)
+				for (int idx = 0; idx < cacheDailyEMA.Length; idx++)
+					if (cacheDailyEMA[idx] != null && cacheDailyEMA[idx].Period == period && cacheDailyEMA[idx].EqualsInput(input))
+						return cacheDailyEMA[idx];
+			return CacheIndicator<LunarTick.DailyEMA>(new LunarTick.DailyEMA(){ Period = period }, input, ref cacheDailyEMA);
+		}
+	}
+}
+
+namespace NinjaTrader.NinjaScript.MarketAnalyzerColumns
+{
+	public partial class MarketAnalyzerColumn : MarketAnalyzerColumnBase
+	{
+		public Indicators.LunarTick.DailyEMA DailyEMA(int period)
+		{
+			return indicator.DailyEMA(Input, period);
+		}
+
+		public Indicators.LunarTick.DailyEMA DailyEMA(ISeries<double> input , int period)
+		{
+			return indicator.DailyEMA(input, period);
+		}
+	}
+}
+
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
+	{
+		public Indicators.LunarTick.DailyEMA DailyEMA(int period)
+		{
+			return indicator.DailyEMA(Input, period);
+		}
+
+		public Indicators.LunarTick.DailyEMA DailyEMA(ISeries<double> input , int period)
+		{
+			return indicator.DailyEMA(input, period);
+		}
+	}
+}
+
+#endregion
